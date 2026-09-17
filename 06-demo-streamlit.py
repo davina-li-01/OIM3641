64e0347146d6ea9d26a244949325678d3e439926
davina-li <dli4@babson.edu>
2026-09-17 11:19:29 -0400

    Streamlit Demo

---
+++ 06-demo-streamlit.py
@@ -0,0 +1,48 @@
+from datetime import date,timedelta
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+import yfinance as yf
+
+END = date.today() - timedelta(days=1)
+START = date.today() - timedelta(days=365)
+
+st.set_page_config(layout="wide",
+                   page_title="Stock Price Analysis",)
+st.title ("Stock Analysis")
+
+st.sidebar.title("Inputs")
+ticker = st.sidebar.text_input("Enter stock ticker",
+                               value='AAPL',)
+col1, col2 = st.sidebar.columns(2)
+start_date = col1.date_input("Start Date", START)
+end_date = col2.date_input("End Date", END)
+
+mv_avg = st.sidebar.slider("Moving Average",
+                           min_value = 0,
+                           max_value = 100,
+                           value = 50,
+                           step = 10)
+
+run_analysis = st.sidebar.button("Run Analysis",
+                                 type = "primary")
+
+def get_stock_data(ticker, start_date, end_date):
+    try:
+        data = yf.download(ticker, start=start_date, end=end_date)
+        if data.empty:
+            return None, f"No data for {ticker}"
+        if isinstance(data.columns, pd.MultiIndex):
+            data.columns = data.columns.get_level_values(0)
+        return data, f"Successfully downloaded data {ticker}"
+    except Exception as e:
+        return None, f"Download failed due to {e}"
+
+
+if run_analysis:
+    data, message = get_stock_data(ticker, start_date, end_date)
+    if data is None:
+        st.error(message)
+    else:
+        st.success(message)
+        st.dataframe(data)
